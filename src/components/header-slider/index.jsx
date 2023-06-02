a6b67d38a24d11731ae0f821c425ee20d98b7810
Gianluca Bredice - Nitro 5 <bredicegian@gmail.com>
2023-06-02 18:57:04 -0300

    [feature] add more pictures for header slider

--- src/components/header-slider/index.jsx
+++ src/components/header-slider/index.jsx
@@ -5,8 +5,12 @@ import React from "react"
 import img1 from "../../assets/images/header/1.png"
 import img2 from "../../assets/images/header/2.png"
 import img3 from "../../assets/images/header/3.png"
+import img4 from "../../assets/images/header/4.png"
+import img5 from "../../assets/images/header/5.png"
+import img6 from "../../assets/images/header/6.png"
 
 const HeaderSlider = () => {
+  const images = [img1, img2, img3, img4, img5, img6]
   return (
     <div className="slider relative z-1">
       <Glider
@@ -18,27 +22,17 @@ const HeaderSlider = () => {
         scrollLock
         dragVelocity={1}
       >
-        <div className="max-h-1">
-          <img
-            className="xs:h-full xl:h-6-5 w-full object-cover"
-            src={img1}
-            alt=""
-          />
-        </div>
-        <div className="max-h-1">
-          <img
-            className="xs:h-full xl:h-6-5 w-full object-cover"
-            src={img2}
-            alt=""
-          />
-        </div>
-        <div className="max-h-1">
-          <img
-            className="xs:h-full xl:h-6-5 w-full object-cover"
-            src={img3}
-            alt=""
-          />
-        </div>
+        {images.map((image, index) => {
+          return (
+            <div key={index} className="max-h-1">
+              <img
+                className="xs:h-full xl:h-6-5 w-full object-cover"
+                src={image}
+                alt=""
+              />
+            </div>
+          )
+        })}
       </Glider>
     </div>
   )
